05dff0dfbf2a3d299ddb30e4386efc4187a10706
MalevolentMoksi <enzomorello.pro@gmail.com>
2026-02-06 23:03:41 +0100

    Github Actions hosts site

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   root: 'src',
-  base: '/',
+  base: '/P2.01-Portfolio/',
   publicDir: '../public',
   
   build: {
